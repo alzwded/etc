94aeb4798e36a765ae5334d833691c83dcf8be89
alzwded <alzwded@gmail.com>
2013-06-18 15:17:55 +0300

    add a script that proves why bash is awesome

---
+++ sedShenanigans/magicBashParamExpansion.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ # this proves why bash is uh... awesome?
+ # lines begining with spaces are meta
+ # prints a non-meta line from this file and executes it
+ l=''
+ sed -e '1 d ; /^  *[^ ].*/d' $0 | \
+     while read line ; do \
+         [ "$line" != '' ] \
+             && echo "# $line" \
+             && eval "$line"\
+         || echo '' ; \
+     done
+ exit 0 # exit main program
+echo ${a:-good}
+echo ${a:=a/b/c.d.e}
+echo ${a:1}
+echo ${a:1:1}
+
+echo ${a%.*} # extensionless
+echo ${a%%.*} # more dramatic
+echo ${a##*/} # tail
+echo ${a#*/} # :-??
+echo ${a%/*} # dir
+
+echo ${a//\//\\\\}
+echo ${a/\//\\\\}
+
+b=AAaa
+echo ${b,,*}
+echo ${b,}
+echo ${b^^*}
+b="hello there!"
+echo ${b^}

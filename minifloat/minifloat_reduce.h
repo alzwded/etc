4fd4f2dfdf19a7312d994a0cc6d5e6690b660e45
Vlad <alzwded@gmail.com>
2013-06-21 06:45:17 -0700

    add header comment

--- minifloat/minifloat_reduce.h
+++ minifloat/minifloat_reduce.h
@@ -1,5 +1,28 @@
+// WARNING: I forget what they're called, but this is like those
+//     C include-macros from yore
 // minifloat_reduce
-// include with MF_RETURN_STATEMENT, MF_DATA and MF_X define
+// include with MF_RETURN_STATEMENT, MF_DATA and MF_X defined
+// parameters:
+//     IN
+//        MF_RETURN_STATEMENT
+//                   method of returning out of the current function
+//                   e.g.: `return' , `return ret'
+//     IN
+//        MF_DATA
+//                   a data_t structure in which we will store the result
+//     IN
+//        MF_X
+//                   the number to normalize
+
+#ifndef MF_RETURN_STATEMENT
+# error "MF_RETURN_STATEMENT not defined!"
+#endif
+#ifndef MF_X
+# error "MF_X not defined!"
+#endif
+#ifndef MF_DATA
+# error "MF_DATA not defined!"
+#endif
 
         if(MF_X <= 7 && MF_DATA.exponent < 1) { 
             MF_DATA.exponent = 0; 

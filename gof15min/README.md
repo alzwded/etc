be71a26e99ffe0c8b157206a4f1a0d8ef979cc5c
Vlad <alzwded@gmail.com>
2013-09-26 18:40:31 +0300

    add comments in fixed version; update README.md

--- gof15min/README.md
+++ gof15min/README.md
@@ -7,7 +7,7 @@ This was the second attempt.
 
 Tactics:
 1.  Implement a quick sparse matrix class of booleans. Quickest way was with a set of pairs representing the _true_ values.
-2.  Use two SparseMatrix's that alternate between odd and even generations. This required a Clear() method and a generation counter.
+2.  Use two SparseMatrix's that alternate between odd and even generations. This required a clear() method and a generation counter.
 3.  Implement the iterative algorithm. Pretty straight forward with the SparseMatrix. Since we don't know any bounds, the matrix needs to keep track of them. This is done in the set() method.
 4.  Implement printing. This is easy.
 5.  Done.
@@ -23,3 +23,22 @@ Working version
 ===============
 
 Bugs were fixed in gof2_fixed.cpp, but this took longer than 15 minutes.
+
+Also, this version has comments.
+
+Compiling
+=========
+
+```sh
+g++ -O3 --std=gnu++11 gof2.cpp && ./a.out < test > log & killall -9 a.out
+```
+
+or
+```sh
+g++ -O3 --std=gnu++11 gof2_fixed.cpp && a.out < test > log & killall -9 a.out
+```
+
+If you're curious about version one
+===================================
+
+It sucked. It took half an hour and it wasn't working. It involved a lot of maps, structs, methods, stuff from stl's algorithm and lambdas. Don't do that if you want to finish fast. Keep it simple, stupid.
--- gof15min/gof2_fixed.cpp
+++ gof15min/gof2_fixed.cpp
@@ -2,7 +2,16 @@
 #include <set>
 #include <utility>
 #include <algorithm>
+#include <climits>
 
+// We need a sparse matrix to keep track of our board
+// This implies we need a way to access the value (i,j) => operator()
+// and a way to clean it => clear()
+// We also need to keep track of our bounds since the board can grow in
+// any direction => {min|max}[xy]
+// Since the board contains only boolean values, the fastest way to
+// implement it is with a set of pair<int, int>. If a pair is in the
+// set, this means that the cell (i, j) is alive
 struct SparseMatrix {
     std::set<std::pair<int, int> > _alive;
     int minx, miny, maxx, maxy;
@@ -15,41 +24,40 @@ struct SparseMatrix {
     }
     void set(int x, int y) {
         _alive.insert(std::make_pair(x, y));
+        // update the bounds after a set
         minx = std::min(minx, x);
         miny = std::min(miny, y);
         maxx = std::max(maxx, x);
         maxy = std::max(maxy, y);
     }
-    void reset(int x, int y) {
-        auto i = _alive.find(std::make_pair(x, y));
-        if(i != _alive.end()) {
-            _alive.erase(i);
-        }
-    }
     void clear() {
         _alive.clear();
-        minx = miny = 32768;
-        maxx = maxy = -32768;
+        minx = miny = INT_MAX;
+        maxx = maxy = INT_MIN;
     }
-} a, b;
+} a, b; // we need to boards, the current and the next generations
 
+// evolve the next generation
 void evolve(int g)
 {
+    // we need to flip boards, easiest way to do it is to keep track
+    // of the generation parity
     SparseMatrix& m = (g % 2) ? a : b;
     SparseMatrix& n = (g % 2) ? b : a;
+    // clear the next generation's board (we don't want residues)
     n.clear();
     
+    // go through the board, the board knows its bounds
     for(int i = m.minx; i <= m.maxx; ++i) {
         for(int j = m.miny; j <= m.maxy; ++j) {
+            // apply the game of life rules
             if(m(i, j)) {
                 int c = 0;
                 if(m(i - 1, j)) c++;
                 if(m(i + 1, j)) c++;
                 if(m(i, j - 1)) c++;
                 if(m(i, j + 1)) c++;
-                if(c < 2 || c == 4) {
-                    continue;
-                } else {
+                if(c >= 2 && c <= 3) {
                     n.set(i, j);
                 }
             } else {
@@ -60,16 +68,17 @@ void evolve(int g)
                 if(m(i, j + 1)) c++;
                 if(c == 3) {
                     n.set(i, j);
-                } else {
-                    continue;
                 }
             }
         }
     }
 }
 
+// print the board
 void print(int g)
 {
+    // again, we need the correct board, we keep track of the generation
+    // number
     SparseMatrix& m = (g % 2) ? a : b;
     for(int i = m.minx; i <= m.maxx; ++i) {
         for(int j = m.miny; j <= m.maxy; ++j) {
@@ -78,18 +87,25 @@ void print(int g)
         }
         printf("\n");
     }
+    // force flush the output since the only way to stop the program
+    // is ^C
     fflush(stdout);
 }
 
 int main()
 {
+    // initialize our boards
     a.clear();
     b.clear();
+    // read the coordinates of living cells
     while(!feof(stdin)) {
         int x, y;
+        // make sure we've actually read something useful
         if(2 == fscanf(stdin, "%d %d", &x, &y))
             a.set(x, y);
     }
+    // keep track of the current generation
+    // it's okay if it overflows, since INT_MAX is odd and INT_MIN is even
     int generation = 0;
     while(1) {
         printf("generation %d\n", generation++);

a04ad31e8e750a100d3e54feb03c41af8e30f4ac
Vlad <alzwded@gmail.com>
2013-10-19 11:41:44 +0300

    add changes

--- khathonscripts/getSome.sh
+++ khathonscripts/getSome.sh
@@ -4,4 +4,5 @@ if [[ $# < 2 ]] ; then
     echo $0 start end
 fi
 
-youtube-dl-2013 'http://www.youtube.com/playlist?list=PL7EF8E7219F8F3C4F' --max-quality=18 -t -R 999 --playlist-start=$1 --playlist-end=$2
+youtube-dl-2013 'http://www.youtube.com/playlist?list=PL7EF8E7219F8F3C4F' -f=18/43/5/17/34 -t -R 999 --playlist-start=$1 --playlist-end=$2
+#youtube-dl-2013 'http://www.youtube.com/playlist?list=PL7EF8E7219F8F3C4F' --max-quality=43 -t -R 999 --playlist-start=$1 --playlist-end=$2
--- khathonscripts/persevere.sh
+++ khathonscripts/persevere.sh
@@ -6,7 +6,7 @@ try=1
 while true ; do
     echo try \#$try
     try=`expr $try + 1`
-	./getSome.sh 59 70 2>&1 | grep ERROR
+	./getSome.sh $1 $2 2>&1 | grep ERROR
     if [[ $?  != 0 ]] ; then
         echo no error
         break

94b54a239a132d4b338da443170765bf092024a9
Vlad <alzwded@gmail.com>
2013-10-19 11:58:31 +0300

    use parameter or env var for the download link

--- khathonscripts/getSome.sh
+++ khathonscripts/getSome.sh
@@ -4,5 +4,4 @@ if [[ $# < 2 ]] ; then
     echo $0 start end
 fi
 
-youtube-dl-2013 'http://www.youtube.com/playlist?list=PL7EF8E7219F8F3C4F' -f=18/43/5/17/34 -t -R 999 --playlist-start=$1 --playlist-end=$2
-#youtube-dl-2013 'http://www.youtube.com/playlist?list=PL7EF8E7219F8F3C4F' --max-quality=43 -t -R 999 --playlist-start=$1 --playlist-end=$2
+youtube-dl-2013 "${3:-$YOUTUBEDLLINK}" -f=18/43/5/17/34 -t -R 999 --playlist-start=$1 --playlist-end=$2

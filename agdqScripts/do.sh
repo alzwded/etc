281e76e2c4b6aa538cfdc321127d7730d049263f
Vlad <alzwded@gmail.com>
2013-11-20 17:50:04 +0200

    update do.sh and enhance.sh
    * in agdqScripts: update do.sh to do extra safe checking
    * in photoProc: update enhance.sh to work with dash

--- agdqScripts/do.sh
+++ agdqScripts/do.sh
@@ -5,7 +5,7 @@ CONVERTER=`[ -x /usr/bin/avconv ] && echo avconv || echo ffmpeg`
 for i in "$@"
 do 
     $CONVERTER -i "$i" -map 0:0 -map 0:1 -acodec copy -vcodec copy "$i.singleTrack.mp4"
-    if [ $? -eq 0 -a -f "$i.singleTrack.mp4" ]
+    if [ $? -eq 0 -a -f "$i.singleTrack.mp4" -a `stat -c %s "$i.singleTrack.mp4"` -gt 4096 ]
     then
         rm "$i"
     else
--- photoProc/enhance.sh
+++ photoProc/enhance.sh
@@ -10,25 +10,25 @@ for i in $@ ; do
     #convert "$i" -normalize -unsharp 0x5+1+0 -scale 1800x1800 "$i"_1
     echo `date +%T` $i : second pass
     convert "$i"_1 -unsharp 0x3+1+0 "$i"_2
-    if [ $? == 0 ] ; then
+    if [ $?  -eq  0 ] ; then
         echo `date +%T` $i : deleting "$i"_1
         rm "$i"_1
     fi
     echo `date +%T` $i : third pass
     convert "$i"_2 -unsharp 0x3+1+0.3 "$i"_3
-    if [ $? == 0 ] ; then
+    if [ $?  -eq  0 ] ; then
         echo `date +%T` $i : deleting "$i"_2
         rm "$i"_2
     fi
     echo `date +%T` $i : fourth pass
     convert "$i"_3 -unsharp 0x3+1+0.3 "$i"_4
-    if [ $? == 0 ] ; then
+    if [ $?  -eq  0 ] ; then
         echo `date +%T` $i : deleting "$i"_3
         rm "$i"_3
     fi
     echo `date +%T` $i : fifth pass
     convert "$i"_4 -unsharp 0x3+1+0.3 "$i"_5.jpg
-    if [ $? == 0 ] ; then
+    if [ $?  -eq  0 ] ; then
         echo `date +%T` $i : deleting "$i"_4
         rm "$i"_4
     fi

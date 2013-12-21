e9a2fd18f69456f44ad4268ea0c272c68676a105
Vlad <alzwded@gmail.com>
2013-12-21 13:18:12 +0200

    add minimum amount of safety and add commented transcoding
    for some reasons that breaks the video :-/

--- vlm/vlmcfggen.pl
+++ vlm/vlmcfggen.pl
@@ -4,12 +4,17 @@ die 'not enough parameters' unless scalar @ARGV > 1;
 
 my $cfgName = shift @ARGV;
 
+die 'cfg file name should start with a dash (-) (safety reasons)' unless ((substr $cfgName,0,1) eq "-");
+
+$cfgName = substr $cfgName, 1;
+
 open A, ">$cfgName";
 
 my $idx = 0;
 foreach $file (@ARGV) {
     print A "new T$idx vod enabled\n";
     print A "setup T$idx input \"$file\"\n";
+    #print A "setup T$idx output #transcode{vcodec=h264,vb=400,width=800}\n";
     ++$idx;
 }
 

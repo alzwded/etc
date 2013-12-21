5a0311806daefd9a686882424d62dd1d153164eb
Vlad <alzwded@gmail.com>
2013-12-21 12:49:29 +0200

    add dox and TODO

--- vlm/vlm.sh
+++ vlm/vlm.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-cvlc -I cli --rtsp-host 0.0.0.0 --rtsp-port 5554 --ttl 2
+# usage:
+# $ vlmcfggen.pl cfg file1.mp4 file2.mp4
+# $ vlm.sh
+# > vlm
+# > load cfg
+#
+# client:
+# $ vlc rtsp://host:5554/T0
+# $ vlc rtsp://host:5554/T1
+#
+# TODO
+# set up transcoding to something that fits in 800x600, ~400kbps or something like that
 
-#echo "this is more of a how-to do it rather than a working script"
-#
-#a="vlm
-#new T vod enabled
-#setup T input \"$1\""
-#
-#echo "$a" | cvlc -I cli --rtsp-host 0.0.0.0 --rtsp-port 5554 --ttl 12
+cvlc -I cli --rtsp-host 0.0.0.0 --rtsp-port 5554 --ttl 2

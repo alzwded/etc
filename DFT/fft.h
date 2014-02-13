9732f104615feb1a19a30640e23cf015cf7c6193
Vlad Meșco <alzwded@gmail.com>
2014-02-13 15:19:02 +0200

    add comment

--- DFT/fft.h
+++ DFT/fft.h
@@ -7,6 +7,10 @@
 extern "C" {
 #endif
 
+// float (*)[2] should correspond to either _Complex float* or 
+//     std::complex<float>* i.e. std::vector<std::complex<float>>.data()
+// and, of course, float (*)[2] should correspond to float[2 * N] or
+//     float [N][2] in classic C89
 void fft(size_t N, float (*X)[2]);
 void ifft(size_t N, float (*X)[2]);
 

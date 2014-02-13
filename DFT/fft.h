24a978d7997d4cec46481e0dd266ac74077d54ed
Vlad Meșco <alzwded@gmail.com>
2014-02-13 15:23:44 +0200

    more comments

--- DFT/fft.h
+++ DFT/fft.h
@@ -7,10 +7,12 @@
 extern "C" {
 #endif
 
-// float (*)[2] should correspond to either _Complex float* or 
-//     std::complex<float>* i.e. std::vector<std::complex<float>>.data()
+// in-place radix-2 DIT FastFourierTransform and InverseFFT
+// the length of the input must be a power of two
+// float (*)[2] should correspond to either _Complex float* (C99) or 
+//     std::complex<float>* i.e. std::vector<std::complex<float>>.data() (C++)
 // and, of course, float (*)[2] should correspond to float[2 * N] or
-//     float [N][2] in classic C89
+//     float [N][2] i.e. typedef float C_t[2]; C_t v[N] in classic C89
 void fft(size_t N, float (*X)[2]);
 void ifft(size_t N, float (*X)[2]);
 

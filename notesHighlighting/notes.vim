6c4c82462c842d563dcb2b8135eb252ab6436257
Vlad Meșco <alzwded@gmail.com>
2014-05-10 14:26:21 +0300

    add highlighted note taking

---
+++ notesHighlighting/notes.vim
@@ -0,0 +1,30 @@
+" Vim syntax file
+" Language: vlad's note taking
+" Version: 1.0
+"
+" How it looks like:
+" # comment
+" ::superbold::
+" ! Subtitle 1 .
+"     a definition := talk talk talk talk ; talk talk talk
+"     some other notes %but this is very important% <- really
+" ! important note !
+
+
+set syntax=
+
+syn match NOTES_spacharacters "[*+-;:]"
+syn match NOTES_def "^.*[:][=]"
+syn region NOTES_bolded matchgroup=NOTES_bolded start="!" end="[!.]"
+syn region NOTES_header matchgroup=NOTES_header start="::" end="::"
+syn region NOTES_important matchgroup=NOTES_important start="%" end="%"
+syn match NOTES_note "[<][-]"
+syn region NOTES_header2 matchgroup=NOTES_header2 start="[#]" end="$"
+
+highlight link NOTES_spacharacters Operator
+highlight link NOTES_def Identifier
+highlight link NOTES_bolded String
+hi link NOTES_header Statement
+hi link NOTES_header2 Comment
+hi link NOTES_important Structure
+hi link NOTES_note Structure
---
+++ notesHighlighting/notes2html.pl
@@ -0,0 +1,164 @@
+#!/usr/bin/perl -w
+
+print "<html><head><style>";
+print 'p { margin-top: 2px; margin-bottom: 2px; }';
+print "</style></head><body style=\"";
+print "font: Monospace, 12;";
+print "\">\n";
+
+my $alreadyPrinted = 0;
+my $convertSpaces = 0;
+my $currentLine = undef;
+my @current = ();
+
+my $state = 0;
+<<EOT
+used to convert *.notes file to html as per notes.vim syntax highlighting
+    0 => default
+    1 => :: ... ::
+    2 => ^ ... :=
+    3 => ! ... ! | ! ... .
+    4 => % ... %
+    5 => # ... \n
+EOT
+;
+
+getnext();
+&defaultp();
+
+sub output {
+    my ($c) = @_;
+    if($c !~ m/[ \t\n]/) { $convertSpaces = 0; }
+    if(!$convertSpaces) { return $c; }
+    if($c eq ' ') { return "&nbsp;"; }
+    if($c eq "\t") { return "&nbsp;" x 4; }
+    return $c;
+}
+
+sub defaultp {
+    while(1) {
+        getnext();
+
+        if($currentLine =~ m/^.*[:][=]/) {
+            &def();
+            $currentLine = join @current;
+            next;
+        } elsif(scalar(@current) >= 2 and $current[0] eq ':' and $current[1] eq ':') {
+            &doublecolon();
+            next;
+        } elsif($current[0] eq '!') {
+            &exclamation();
+            next;
+        } elsif($current[0] eq '%') {
+            &percent();
+            next;
+        } elsif($current[0] eq '#') {
+            &comment();
+            next;
+        }
+
+        getnext();
+
+        my $c = $current[0];
+        if($c =~ m|[.,;/*\-+]|) {
+            print '<span style="font-weight: bold; color: red;">' . &output($c) . '</span>';
+        } else {
+            print &output($c);
+        }
+
+        shift @current;
+
+    }
+}
+
+sub doublecolon {
+    print '<span style="font-weight: bold; font-size: larger;">';
+    shift @current;
+    shift @current;
+    while(1) {
+        getnext();
+        if(scalar(@current) > 2 and $current[0] eq ':' and $current[1] eq ':') {
+            print("</span>");
+            shift @current;
+            shift @current;
+            return;
+        }
+        print &output(shift @current);
+    }
+}
+
+sub def {
+    print '<span style="font-weight: bold; color: red;">';
+    while(1) {
+        getnext();
+        if($current[0] eq ':' and $current[1] eq '=') {
+            print(":=</span>");
+            shift @current;
+            shift @current;
+            return;
+        }
+        print &output(shift @current);
+    }
+}
+
+sub exclamation {
+    print '<span style="font-weight: bold; color: navy;">';
+    shift @current;
+    while(1) {
+        getnext();
+        if($current[0] eq '!' or $current[0] eq '.') {
+            print("</span>");
+            shift @current;
+            return;
+        }
+        print &output(shift @current);
+    }
+}
+
+sub percent {
+    print '<span style="font-weight: bold;"><i>';
+    shift @current;
+    while(1) {
+        getnext();
+        if($current[0] eq '%') {
+            print("</i></span>");
+            shift @current;
+            return;
+        }
+        print &output(shift @current);
+    }
+}
+
+sub comment {
+    print '<span style="color: #404040;"><i><sub>';
+    shift @current;
+    while(1) {
+        getnext();
+        if($current[0] eq "\n") {
+            print("</sub></i></span>");
+            shift @current;
+            return;
+        }
+        print &output(shift @current);
+    }
+}
+
+sub getnext {
+    if(scalar(@current) == 0) {
+        if($alreadyPrinted) { print("</p>\n"); }
+        else { $alreadyPrinted = 1; }
+        $currentLine = <STDIN>;
+        if(!$currentLine) { &doexit(); }
+        @current = split //, $currentLine;
+        print('<p>');
+        $convertSpaces = 1;
+    } else {
+        #print defined(@current);
+        #print "\n" . scalar(@current) . "\n";
+    }
+}
+
+sub doexit {
+    print '</body></html>'."\n";
+    exit 0;
+}

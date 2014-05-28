f80f0bc6878c50914a244a8a42c9f8194d555b74
Vlad Meșco <alzwded@gmail.com>
2014-05-28 13:30:19 +0300

    categorize books by first letter of title

---
+++ manybooks-downloader/splitter.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use File::Path qw/make_path/;
+use File::Copy qw/move/;
+
+die 'splitter [-d target] file1 file2 ...' unless(scalar @ARGV > 0);
+
+my $targetDir = ".";
+if($ARGV[0] eq '-d') {
+    $targetDir = $ARGV[1] or die;
+    shift @ARGV;
+    shift @ARGV;
+}
+
+my @files = @ARGV;
+my %dests = map { $_ => "$targetDir/".lc(substr $_, 0, 1)."/" ; } @files;
+
+foreach (keys %dests) {
+    my $file = $_;
+    my $dest = $dests{$file};
+    $dest =~ s#/ /#/Misc/#g;
+    print "$file => $dest\n";
+    if(not -d $dest) {
+        make_path($dest) == 1 or die;
+    }
+    move($file, $dest) or die;
+}

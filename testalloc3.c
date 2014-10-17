17a7f32e793b6aa1b1ac63e7e8bea1b9f7e69eb8
Vlad <alzwded@gmail.com>
2014-10-17 14:37:54 +0300

    completed removing all structured programming constructs

--- testalloc3.c
+++ testalloc3.c
@@ -29,16 +29,33 @@
 #include <string.h>
 #include <stdint.h>
 
-typedef int16_t type_t;
-typedef type_t* ptr_t;
-
-#define TOTAL (64*1024)//(4096)
-#define ALLOCTAB (3200)//(200)
-#define BLOCK (8)
-
+// represent the SWAP operation as a macro since the JakVMhs swap
+//     code bears no resemblance to C code
 #define SWAPT type_t
 #define SWAP(a, b) do{ SWAPT t = a; a = b; b = t; }while(0)
 
+// represent conditional jumps with macros since assembly tends
+//     to be somewhat different from structured code
+#define WHEN(X) if(X)
+#define JUMP(X) goto X
+
+// configuration constants; theoretically determined at compile time
+// BASEADDR: the base address of the malloc area as an offset of words; 
+//           everything that's before it is static program data
+#define BASEADDR (3)
+// TOTAL: the total amount of memory, 64k words
+//        well, actually it can be considered to be the 'right' marker
+//        of the malloc area if you consider BASEADDR to be the 'left'
+#define TOTAL (64*1024)
+// ALLOCTAB: the size of the allocation table, in words
+//           memory will be allocated starting from BASEADDR + ALLOCTAB
+#define ALLOCTAB (3200)
+// BLOCK: the size of an allocation block, in number of words
+#define BLOCK (8)
+
+// a representation of the memory
+typedef int16_t type_t;
+typedef type_t* ptr_t;
 type_t memory[TOTAL];
 
 // this would become wrapped in a
@@ -49,10 +66,10 @@ ptr_t dalloc(int size)
 {
 // the variables we're using
     int addr = 0;
-    size_t i = -1, j;
+    size_t i = BASEADDR-1, j;
 
 // jump into the main body of the function
-    goto dalloc_mainLoop;
+    JUMP(dalloc_mainLoop);
 
 // entered when a slot is occupied
 dalloc_occupied:
@@ -65,22 +82,25 @@ dalloc_mainLoop:
     // look at the next slot
     ++i;
     // if it's occupied, handle it
-    if(memory[i] < 0) goto dalloc_occupied;
+    WHEN(memory[i] < 0)
+    JUMP(dalloc_occupied);
     // if it's an exact match, handle it
-    if(memory[i] == size) goto dalloc_foundMatching;
+    WHEN(memory[i] == size)
+    JUMP(dalloc_foundMatching);
     // if there's room, handle it
-    if(memory[i] > size) goto dalloc_foundMore;
+    WHEN(memory[i] > size)
+    JUMP(dalloc_foundMore);
     // otherwise move the potential pointer by that amount
     addr += memory[i];
     // and loop
-    goto dalloc_mainLoop;
+    JUMP(dalloc_mainLoop);
 
 // entered when an exact size match is found
 dalloc_foundMatching:
     // mark it as occupied
     memory[i] = -memory[i];
     // compute the pointer address and return
-    return &memory[addr * BLOCK + ALLOCTAB];
+    return &memory[BASEADDR + addr * BLOCK + ALLOCTAB];
 
 // entered when a slot was found that is big enough
 dalloc_foundMore:
@@ -98,23 +118,27 @@ dalloc_foundMore:
     memory[i] = -size;
     // unleash the push back algorithm;
     // 0 is the terminator
-    while(buf != 0) {
-        // move to the next slot
-        ++j;
-        // swap out buf and the slot
-        SWAP(buf, memory[j]);
-    }
+dalloc_pushBackStart:
+    WHEN(buf == 0)
+    JUMP(dalloc_pushBackEnd);
+    // move to the next slot
+    ++j;
+    // swap out buf and the slot
+    SWAP(buf, memory[j]);
+    // loop
+    JUMP(dalloc_pushBackStart);
+dalloc_pushBackEnd:
     // compute the pointer address and return
-    return &memory[addr * BLOCK + ALLOCTAB];
+    return &memory[BASEADDR + addr * BLOCK + ALLOCTAB];
 }
 
 void dfree(ptr_t p)
 {
 // the variables we're using
-    int base = (p - memory - ALLOCTAB) / BLOCK;
-    size_t i = -1, j, o = 0;
+    int base = (p - memory - BASEADDR - ALLOCTAB) / BLOCK;
+    size_t i = BASEADDR-1, j, o = 0;
 
-    goto dfree_mainLoop;
+    JUMP(dfree_mainLoop);
 
 // entered when a slot is occupied
 dfree_occupied:
@@ -125,21 +149,25 @@ dfree_mainLoop:
     // move to the next slot
     ++i;
     // if it's occupied, deal with it
-    if(memory[i] > 0) goto dfree_occupied;
+    WHEN(memory[i] > 0)
+    JUMP(dfree_occupied);
     // decrement our debt
     base -= -memory[i];
     // if we still have debt, move on
     // 0 is still debt because reasons; magic number ho
-    if(base >= 0) goto dfree_mainLoop;
+    WHEN(base >= 0)
+    JUMP(dfree_mainLoop);
     // else, mark the memory as free
     memory[i] = -memory[i];
 //
 // normalize
 //
     // if we have a free slot on the left, merge with it
-    if(i > 0 && memory[i - 1] > 0) goto dfree_haveLeft;
+    WHEN(i > BASEADDR && memory[i - 1] > 0)
+    JUMP(dfree_haveLeft);
     // if we have a free slot on the right, merge with it
-    if(memory[i + 1] >= 0) goto dfree_haveRight;
+    WHEN(memory[i + 1] >= 0)
+    JUMP(dfree_haveRight);
     // nothing to normalize
     return;
 
@@ -148,13 +176,14 @@ dfree_haveRight:
     memory[i] += memory[i + 1];
     // jump to the move code
     j = i + 2;
-    goto dfree_toEndWithIt;
+    JUMP(dfree_toEndWithIt);
 
 dfree_haveLeft:
     // add the value of this slot to the left slot
     memory[i - 1] += memory[i];
     // if we don't have a free slot to our right, skip some code
-    if(memory[i + 1] <= 0) goto dfree_noFreeRight;
+    WHEN(memory[i + 1] <= 0)
+    JUMP(dfree_noFreeRight);
     // we have a free slot on the right; merge it with the left slot
     memory[i - 1] += memory[i + 1];
     // configure toEndWithIt to reach a slot farther to its right
@@ -165,16 +194,24 @@ dfree_noFreeRight:
     j = i + 1;
     // fall through to dfree_toEndWithIt
 dfree_toEndWithIt:
-    for(; memory[j - 1]; ++j) {
-        memory[j - 1] = memory[j + o];
-    }
+    // if we're at the end of the malloc memory, everything was moved
+    WHEN(memory[j - 1] == 0)
+    JUMP(dfree_toEndWithItEnd);
+    // move the slot further down the road into our house
+    memory[j - 1] = memory[j + o];
+    // move to the next slot
+    ++j;
+    // and repeat
+    JUMP(dfree_toEndWithIt);
+dfree_toEndWithItEnd:
+    // done!
     return;
 }
 
 int main(int argc, char* argv[])
 {
     memset(&memory[0], 0, sizeof(memory));
-    memory[0] = (TOTAL - ALLOCTAB) / BLOCK;
+    memory[BASEADDR] = (TOTAL - ALLOCTAB - BASEADDR) / BLOCK;
 
 #define SAY(STR) do{\
     printf("%s\n", STR); \
@@ -188,6 +225,10 @@ int main(int argc, char* argv[])
     printf("\n"); \
     printf("    PWORD%5d PCHAR%5d\n", VAR - memory, (char*)VAR - (char*)memory); \
 }while(0)
+
+    for(size_t i = 0; i < BASEADDR; memory[i++] = (i%2)?-9999:9999)
+        ;
+    SAY("+-9999 is a momentarily reserved value for testing purposes\nconsider them cannaries");
 
     printf("=============\n");
     SAY("initial");

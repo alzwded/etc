9e20f61e4d295484f877242cfacc1a45c1eacf03
Vlad Meșco <alzwded@gmail.com>
2015-04-24 16:01:37 +0300

    reduce

--- AtExitDo/AtExitDo.hpp
+++ AtExitDo/AtExitDo.hpp
@@ -3,21 +3,25 @@
 
 // include & compile with g++ --std=gnu++14
 
+// VS: apparently can only be compiled with VS2015
+//     there's a bug related to variadic template parameter
+//     expansion in 2013
+
 #include <functional>
 
 template<typename T, typename... TArg>
 struct AtExit_s {
     std::function<void(void)> func_;
-    AtExit_s(T& func, TArg&... data)
-    : func_([&]() { func(data...); })
-    {}
+
     AtExit_s(T&& func, TArg&&... data)
     : func_([&]() { func(data...); })
     {}
+
     ~AtExit_s()
     {
         func_();
     }
+    AtExit_s(AtExit_s&) = delete;
     AtExit_s(AtExit_s&& other)
     : func_(other.func_)
     {
@@ -33,13 +37,7 @@ struct AtExit {
     template<typename T, typename... TArg>
     static inline AtExit_t<T, TArg...> Do(T&& func, TArg&&... data)
     {
-        return std::move(AtExit_t<T, TArg...>(func, data...));
-    }
-
-    template<typename T, typename... TArg>
-    static inline AtExit_t<T, TArg...> Do(T& func, TArg&... data)
-    {
-        return std::move(AtExit_t<T, TArg...>(func, data...));
+        return std::move(AtExit_t<T, TArg...>(std::forward<decltype(func)>(func), std::forward<decltype(data)>(data)...));
     }
 };
 

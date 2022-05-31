a966850e56eb46e27168df1fed5fd8fb8922e3c4
Vlad Meșco <alzwded@gmail.com>
2022-05-31 17:38:38 +0300

    add 2clause bsd

--- sumnumbers/sumnumbers.c
+++ sumnumbers/sumnumbers.c
@@ -1,3 +1,14 @@
+/*
+Copyright 2022 Vlad Mesco
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <unistd.h>
 #include <stdio.h>
 #include <getopt.h>
@@ -15,6 +26,7 @@ __attribute__((noreturn)) void usage(const char* argv0)
     printf("Usage: %s [-%s] [numbers....]\n", argv0, OPTSTRING);
     printf("    -h      prints this message\n");
     printf("    -e      returns sum as error code instead of printing to stdout\n");
+    printf("            this rounds to the nearest int\n");
     printf("    If numbers are supplied as args, it prints the sum and exits\n");
     printf("    Else, reads numbers from stdin, prints the sum and exits\n");
     printf("    This uses floating point (double) internally\n");

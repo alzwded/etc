64760d344a8fa7ead5307cefe0c7b03618511269
Vlad Meșco <alzwded@gmail.com>
2024-10-29 16:46:35 +0200

    add prepdvd.sh script to prepare a dvd image from audio

---
+++ prepdvd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ $# < 1 ]] ; then
+    echo "Usage: $0 song1 song2 ..."
+    exit 2
+fi
+
+for i in "$@" ; do
+    ffmpeg -y -f lavfi -i color=size=720x480:rate=25:color=black -i "$i" -c:a pcm_s16le -shortest -c:v libxvid "${i%.*}.mov"
+done
+
+echo "----------------------------------------"
+echo "Use DeVeDe to create a Video DVD project"

a10a70653141d736e708979231ffaa74436f5231
Vlad Meșco <alzwded@gmail.com>
2024-10-29 17:36:21 +0200

    udpate prepdvd

--- prepdvd.sh
+++ prepdvd.sh
@@ -9,6 +9,8 @@ for i in "$@" ; do
     ffmpeg -y -f lavfi -i color=size=720x480:rate=25:color=black -i "$i" -c:a pcm_s16le -shortest -c:v libxvid "${i%.*}.mov"
 done
 
-echo "----------------------------------------"
-echo "Use DeVeDe to create a Video DVD project"
+echo "-----------------------------------------"
+echo "Use DeVeDe to create a Video DVD project-"
 echo "(add a menu with a playall option...)"
+echo "(and set properties to go through titles)"
+echo "(and have the last one loop back)"
